386ea5cf4d318101369d834b6f25c5b3ad59b23b
masifmirza927 <masifmirza927@gmail.com>
2023-06-01 12:07:09 +0500

    ratings

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -18,6 +18,15 @@ function ProductDetails() {
     }, []);
 
 
+    let ratingJSX = "";
+
+    if (Object.keys(product).length > 0) {
+        let Rate = Number(product.rating['rate']);
+        for (let i = 0; i < Math.round(Rate); i++) {
+            ratingJSX += `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style={{fill: "rgba(0, 0, 0, 1)"}}><path d="M21.947 9.179a1.001 1.001 0 0 0-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 0 0-1.822-.001L8.622 8.05l-5.701.453a1 1 0 0 0-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 0 0 1.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 0 0 1.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z"></path></svg>`
+        }
+    }
+
     if (Object.keys(product).length > 0) {
         return (
             <>
@@ -31,7 +40,7 @@ function ProductDetails() {
                             <p>{product.description}</p>
                             <div><b>Category: </b>{product.category}</div>
                             <div><b>Price: </b>${product.price}</div>
-                            <div>{product.rating['rate']}</div>
+                            <div dangerouslySetInnerHTML={{ __html: ratingJSX }} />
                         </div>
                     </div>
                 </div>
